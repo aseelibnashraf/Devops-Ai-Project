259fbee37d8751d68456687e61dab9ab262fb047
Mohamed Aseel <aseel@Aseels-MacBook-Air.local>
2026-09-15 12:36:12 +0530

    day2: intentionally break test

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     client = app.test_client()
     resp = client.get("/")
-    assert resp.status_code == 200
+    assert resp.status_code == 404  # changed from 200 - this will fail
